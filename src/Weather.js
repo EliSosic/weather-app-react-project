62310ec19e6b462964fbe9ad6f196a2f45a11664
Elisa <elisosic@gmail.com>
2024-04-26 21:24:02 +0200

    updated css

--- src/Weather.js
+++ src/Weather.js
@@ -5,17 +5,16 @@ export default function Weather () {
     return(
         <div className="Weather">
             <form className="text-center">
-                <input type="search" placeholder="Enter a city"/>
-                <input type="search" value="Search" />
+                <input type="search" className="Input-box" placeholder="Enter a city"/>
+                <input type="search" className="Search-button"value="Search" />
             </form>
-
-            <h2>Trieste</h2>
             
             <div className="row">
                 <div className="col-6">
-                    <div> 23.03.2024</div>
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png" alt="weather description"/>
-                    <div> Mostly cloudy</div>
+                    <h2> Trieste</h2>
+                    <div className="Main-date"> 23.03.2024</div>
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png" alt="weather description" className="img-fluid"/>
+                    <div className="Main-description"> Mostly cloudy</div>
                 </div>
                 <div className="col-6">
                     <div className="Main-temperature"> 29°C</div>
